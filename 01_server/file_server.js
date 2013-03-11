c07828c8cfc266df8102e0ddb2080a2c5323b8b4
Sho Hashimoto <hashimoto@shokai.org>
2013-03-12 05:42:20 +0900

    p15 - cacheAndDeliver

--- 01_server/file_server.js
+++ 01_server/file_server.js
@@ -8,12 +8,25 @@ var mimeTypes = {
   '.css' : 'text/css'
 };
 
+var cache = {};
+var cacheAndDeliver = function(fname, callback){
+  if(cache[fname]){
+    console.log('cache hit : ' + fname);
+    callback(null, cache[fname].content);
+    return;
+  }
+  fs.readFile(fname, function(err, data){
+    if(!err) cache[fname] = {content: data};
+    callback(err, data);
+  });
+};
+
 http.createServer(function(req, res){
   var lookup = path.basename(decodeURI(req.url)) || 'index.html';
   var file = 'content/' + lookup;
   fs.exists(file, function(exists){
     if(exists){
-      fs.readFile(file, function(err, data){
+      cacheAndDeliver(file, function(err, data){
         if(err){
           res.writeHead(500);
           res.end('internal server error!!');
