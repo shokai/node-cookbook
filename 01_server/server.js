cc6929390363c230a4f700670569e34aff7d852f
Sho Hashimoto <hashimoto@shokai.org>
2013-03-12 02:10:04 +0900

    p3 - parse request path

--- 01_server/server.js
+++ 01_server/server.js
@@ -1,6 +1,23 @@
 var http = require('http');
+var path = require('path');
+
+var pages = [
+  {route: '', output: 'zanmai'},
+  {route: 'about', output: 'sample server app'},
+  {route: 'another page', output: function(){ return 'this is ' + this.route; }}
+];
 
 http.createServer(function(req, res){
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Woohoo!');
+  var lookup = path.basename(decodeURI(req.url));
+  pages.forEach(function(page){
+    if(page.route === lookup){
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(typeof page.output === 'function' ? page.output() : page.output);
+    }
+  });
+  if(!res.finished){
+    res.writeHead(404);
+    res.end('404 page not found');
+  }
 }).listen(30000);
+
